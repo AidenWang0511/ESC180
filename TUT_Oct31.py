732ed5241b7392f24330a4be75f0b40ce7241b77
AidenWang0511 <aidenwang0511@gmail.com>
2022-10-31 17:09:29 -0400

    ihk

--- TUT_Oct31.py
+++ TUT_Oct31.py
@@ -1,3 +1,23 @@
 f = open("data2.txt")
-text = f.read()
-print(text)
+text = f.readlines()
+for line in text:
+    if ("lol") in line.lower():
+        print(line[:-1])
+
+def dict_to_str(d):
+    res = ""
+    for k,v in d.items():
+        res += str(k) + ", " + str(v) + "\n"
+    return res[:-1]
+
+def dict_to_str_sorted(d):
+    l = sorted(d)
+    res = ""
+    for k in l:
+        res += str(k) + ", " + str(d[k]) + "\n"
+    return res[:-1]
+
+print(dict_to_str({1:2,2:3,5:6}))
+print("--------------")
+print(dict_to_str_sorted({6:2,2:3,10:6}))
+
