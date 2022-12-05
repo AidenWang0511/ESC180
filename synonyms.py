ae7619bb0d45ae2e4b8a202fd9ff219674a98b01
AidenWang0511 <70038087+AidenWang0511@users.noreply.github.com>
2022-12-05 03:14:19 -0500

    Update synonyms.py

--- synonyms.py
+++ synonyms.py
@@ -4,6 +4,7 @@ Author: Qixian Aiden Wang, Gary Yang Last modified: Dec. 5, 2022.
 '''
 
 import math
+import time
 
 def norm(vec):
     '''Return the norm of a vector stored as a dictionary, as
@@ -16,7 +17,13 @@ def norm(vec):
 
     return math.sqrt(sum_of_squares)
 
-
+'''
+Calculate the cosine similarity given 2 vectors
+Parameters:
+    vec1 - list representing vector 1
+    vec2 - list representing vector 2
+return the cosine similarity
+'''
 def cosine_similarity(vec1, vec2):
     sum = 0
     for k in vec1.keys():
@@ -24,7 +31,12 @@ def cosine_similarity(vec1, vec2):
             sum += vec1[k] * vec2[k]
     return sum / (norm(vec1) * norm(vec2))
 
-
+'''
+Build the semantic descriptor of a passage
+Parameters:
+    sentences - 2D list storing a passage
+return a dictionary representing the semantic descriptor given a passage
+'''
 def build_semantic_descriptors(sentences):
     sd = {}
     for l in sentences:
@@ -41,6 +53,13 @@ def build_semantic_descriptors(sentences):
                         sd[word][w] += 1
     return sd
 
+
+'''
+Put given files into semantic descriptor
+Parameters:
+    filenames - storing list of file names
+return a dictionary representing the semantic descriptor given multiple files
+'''
 def build_semantic_descriptors_from_files(filenames):
     txt = ""
     for i in filenames:
@@ -65,7 +84,15 @@ def build_semantic_descriptors_from_files(filenames):
 
     return build_semantic_descriptors(txt)
 
-
+'''
+From a list of words, find the word with largest semantic similarity to a given word
+Parameters:
+    word - the target word
+    choices - list of possible similar words
+    semantic_descriptors - dictionary
+    similarity_fn - the similarity function that will be used
+return the word with largest semantic similarity
+'''
 def most_similar_word(word, choices, semantic_descriptors, similarity_fn):
     res = choices[0]
     word = word.lower()
@@ -85,7 +112,14 @@ def most_similar_word(word, choices, semantic_descriptors, similarity_fn):
             res = choices[i]
     return res
 
-
+'''
+Test how accurate is the semantic similarity method given certain files.
+Parameters:
+    filename - file name storing the test questions
+    semantic_descriptors - dictionary
+    similarity_fn - the similarity function that will be used
+return the % of questions that are correct
+'''
 def run_similarity_test(filename, semantic_descriptors, similarity_fn):
     numTrue = 0
     f = open(filename, "r", encoding = "latin1").read()
@@ -104,7 +138,13 @@ def run_similarity_test(filename, semantic_descriptors, similarity_fn):
     return numTrue / len(questions) * 100.0
 
 
-
+if __name__ == '__main__':
+    st = time.time()
+    sem_descriptors = build_semantic_descriptors_from_files(["wp.txt", "sw.txt"])
+    fin = time.time()
+    res = run_similarity_test("test.txt", sem_descriptors, cosine_similarity)
+    print("time taken:", fin-st)
+    print(res, "of the guesses were correct")
 
 
 
